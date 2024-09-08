da35ca028a6b304c7cdc73097af2f7abef8beae4
davidting0918 <dting0918@gmail.com>
2024-09-08 16:01:01 +0800

    Add api document

--- app/test_ticket_routes.py
+++ app/test_ticket_routes.py
@@ -73,7 +73,7 @@ class TestTicketRoutes(unittest.TestCase):
 
         self.loop.run_until_complete(run_test())
 
-    def test_update_post_ticket_info(self):
+    def test_approve_ticket(self):
         async def run_test():
             async with AsyncClient(app=app, base_url=self.base_url) as ac:
                 # test create a new ticket in the db
@@ -107,6 +107,56 @@ class TestTicketRoutes(unittest.TestCase):
                 # test approve
                 params = ApproveRejectParams(ticket_id=ticket_id, user_id="dev-test")
                 res = await ac.post("/tickets/approve", json=params.model_dump(), headers=self.headers)
+                self.assertEqual(res.status_code, 200, f"Response: {res.json()}")
+                data = res.json()
+                self.assertEqual(data["status"], 1)
+                self.assertEqual(data["data"]["status"], "approved")
+
+                # test delete the ticket in the db
+                params = DeleteTicketParams(ticket_id=ticket_id)
+                res = await ac.post("/tickets/delete", json=params.model_dump(), headers=self.headers)
+                self.assertEqual(res.status_code, 200, f"Response: {res.json()}")
+                data = res.json()
+                self.assertEqual(data["data"]["delete_status"], True)
+
+                return
+
+        self.loop.run_until_complete(run_test())
+
+    def test_reject_ticket(self):
+        async def run_test():
+            async with AsyncClient(app=app, base_url=self.base_url) as ac:
+                # test create a new ticket in the db
+                test_annc = Announcement(
+                    annc_type="text",
+                    content_text="This is a test announcement",
+                    content_html="<p>This is a test announcement</p>",
+                    content_md="This is a test announcement",
+                    category="test",
+                    language="en",
+                    label=["test"],
+                    chats=["test-123"],
+                    actual_chats=["test-123"],
+                )
+
+                test_post_ticket = PostTicket(
+                    annc=test_annc,
+                    action="post_annc",
+                    status="pending",
+                    creator_id="test-123",
+                    creator_name="test",
+                )
+
+                params = CreateTicketParams(ticket=test_post_ticket)
+                ticket_id = params.ticket.ticket_id
+                res = await ac.post("/tickets/create", json=params.model_dump(), headers=self.headers)
+                self.assertEqual(res.status_code, 200, f"Response: {res.json()}")
+                data = res.json()
+                self.assertEqual(data["status"], 1)
+
+                # test approve
+                params = ApproveRejectParams(ticket_id=ticket_id, user_id="dev-test")
+                res = await ac.post("/tickets/reject", json=params.model_dump(), headers=self.headers)
                 self.assertEqual(res.status_code, 200, f"Response: {res.json()}")
                 data = res.json()
                 self.assertEqual(data["status"], 1)
